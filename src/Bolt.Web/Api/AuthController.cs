bee837fafebf71bb09b9d5731c567f45476e365f
Shaked Hadas <shakedh100@gmail.com>
2021-02-22 20:37:33 +0200

    implemented client side authentication

---
+++ src/Bolt.Web/Api/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bolt.Core.Entities;
+using Bolt.Web.ApiModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bolt.Web.Api
+{
+    public class AuthController : BaseApiController
+    {
+        [HttpGet("google")]
+        public async Task<IActionResult> GoogleAuth([FromBody] GoogleAuthDTO auth)
+        {
+            // TODO: implement token check
+
+            return Ok();
+        }
+    }
+
+    public class GoogleAuthDTO
+    {
+        public string TokenId { get; set; }
+    }
+}
--- src/Bolt.Web/wwwroot/src/components/App.tsx
+++ src/Bolt.Web/wwwroot/src/components/App.tsx
@@ -1,28 +1,63 @@
 import React, { FC } from 'react';
 import {
-  Layout, Menu, Typography,
+  Button,
+  Col,
+  Layout, Menu, Row, Typography,
 } from 'antd';
 import './App.css';
 import {
   BrowserRouter, Link, Route, Switch,
 } from 'react-router-dom';
 import 'reflect-metadata';
+import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import axios from 'axios';
 import Homepage from './Homepage';
 import Restaurant from './Restaurant';
 
 const { Header, Content } = Layout;
-const { Title } = Typography;
+
+const Logo = styled.img`
+  height: 64px;
+`;
 
 const App: FC = () => {
+  const onLoginSuccess = (res : GoogleLoginResponse | GoogleLoginResponseOffline) => {
+    if ('googleId' in res) {
+      axios.post('', { tokenId: res.tokenId });
+    }
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
         <Layout>
           <Header>
-            <img className="logo" src="/logo_transparent.png" alt="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-            </Menu>
+            <Row justify="space-around">
+              <Col xs={6} sm={6} md={4} lg={4} xl={3} xxl={2}>
+                <Logo src="/logo_transparent.png" alt="logo" />
+              </Col>
+              <Col xs={6} sm={14} md={16} lg={16} xl={18} xxl={16}>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                  <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+                </Menu>
+              </Col>
+              <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4}>
+
+                <GoogleLogin
+                  clientId="489163125564-u2h253prvg1tg3tlfq712gfmijkjsrcv.apps.googleusercontent.com"
+                  render={
+                  (props) => (
+                    <Button onClick={props.onClick} disabled={props.disabled} icon={<FontAwesomeIcon icon={faGoogle} />}>Login With Google</Button>
+                  )
+                }
+                  onSuccess={onLoginSuccess}
+                />
+              </Col>
+
+            </Row>
           </Header>
           <Content style={{ padding: '25px 50px', minHeight: 'calc(100vh - 64px)' }}>
             <Switch>
