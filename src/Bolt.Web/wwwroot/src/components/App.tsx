3809f01218096ae84ab297456784b2a7258687d6
Shaked Hadas <shakedh100@gmail.com>
2021-02-23 13:21:27 +0200

    Showed an avatar when user is logged in

--- src/Bolt.Web/wwwroot/src/components/App.tsx
+++ src/Bolt.Web/wwwroot/src/components/App.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
+  Avatar,
   Button,
   Col,
-  Layout, Menu, Row, Typography,
+  Layout, Menu, Row, Space, Typography,
 } from 'antd';
 import './App.css';
 import {
@@ -20,18 +21,24 @@ import Restaurant from './Restaurant';
 import { User } from '../models/User';
 
 const { Header, Content } = Layout;
+const { Text } = Typography;
 
 const Logo = styled.img`
   height: 64px;
 `;
 
+const WhiteText = styled(Text)`
+  color: white
+`;
+
 const App: FC = () => {
+  const [loggedUser, setLoggedUser] = useState<User>();
+
   const onLoginSuccess = async (res : GoogleLoginResponse | GoogleLoginResponseOffline) => {
     if ('googleId' in res) {
       const { data } = await axios.post<User>('/api/Auth/google', { tokenId: res.tokenId });
       const user = plainToClass(User, data);
-
-      console.log(user);
+      setLoggedUser(user);
     }
   };
 
@@ -49,18 +56,30 @@ const App: FC = () => {
                   <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
                 </Menu>
               </Col>
-              <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4}>
-
-                <GoogleLogin
-                  clientId="489163125564-u2h253prvg1tg3tlfq712gfmijkjsrcv.apps.googleusercontent.com"
-                  render={
-                  (props) => (
-                    <Button onClick={props.onClick} disabled={props.disabled} icon={<FontAwesomeIcon icon={faGoogle} />}>Login With Google</Button>
+              {
+                !loggedUser
+                  ? (
+                    <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4}>
+                      <GoogleLogin
+                        clientId="489163125564-u2h253prvg1tg3tlfq712gfmijkjsrcv.apps.googleusercontent.com"
+                        render={
+                          (props) => (
+                            <Button onClick={props.onClick} disabled={props.disabled} icon={<FontAwesomeIcon icon={faGoogle} />}>Login With Google</Button>
+                          )
+                        }
+                        onSuccess={onLoginSuccess}
+                      />
+                    </Col>
                   )
-                }
-                  onSuccess={onLoginSuccess}
-                />
-              </Col>
+                  : (
+                    <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4}>
+                      <Space direction="horizontal">
+                        <WhiteText>{`Hello ${loggedUser.firstName} ${loggedUser.lastName}`}</WhiteText>
+                        <Avatar src={loggedUser.imageUrl} />
+                      </Space>
+                    </Col>
+                  )
+              }
 
             </Row>
           </Header>
